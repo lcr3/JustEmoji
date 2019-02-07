5fc59e272de721bd302b68bdb6c1bfcd65f6d225
ryookano <okano@lassic.co.jp>
2019-02-07 23:39:01 +0900

    Initial Commit

---
+++ JustEmoji/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JustEmoji
+//
+//  Created by okano on 2019/02/07.
+//  Copyright © 2019 ryookano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JustEmoji/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JustEmoji
+//
+//  Created by okano on 2019/02/07.
+//  Copyright © 2019 ryookano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
